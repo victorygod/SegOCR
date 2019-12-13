87e786236bb295179ba09ac4fe1f936b6d44eb4f
Hantian Li <htli0719@gmail.com>
2019-12-13 12:11:10 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,139 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+import os
+import multiprocessing
+import util
+
+class Unet:
+    def __init__(self):
+        pass
+
+    def _conv(self, x, kernels, istraining):
+        x = tf.keras.layers.Conv2D(kernels, (3, 3), padding = "same")(x)
+        x = tf.keras.layers.BatchNormalization()(x, training = istraining)
+        x = tf.nn.relu(x)
+        return x
+
+    def build(self, input_tensor, istraining = True):
+        x = self._conv(input_tensor, 64, istraining)
+        conv1 = self._conv(x, 64, istraining)
+        shape1 = tf.shape(conv1)
+        x = tf.keras.layers.MaxPool2D(padding="same")(conv1)
+
+        x = self._conv(x, 64, istraining)
+        conv2 = self._conv(x, 64, istraining)
+        shape2 = tf.shape(conv2)
+        x = tf.keras.layers.MaxPool2D(padding="same")(conv2)
+
+        x = self._conv(x, 64, istraining)
+        conv3 = self._conv(x, 64, istraining)
+        shape3 = tf.shape(conv3)
+        x = tf.keras.layers.MaxPool2D(padding="same")(conv3)
+
+        x = self._conv(x, 64, istraining)
+        x = self._conv(x, 64, istraining)
+        conv4 = tf.keras.layers.Dropout(0.5)(x)
+        shape4 = tf.shape(conv4)
+        x = tf.keras.layers.MaxPool2D(padding="same")(conv4)
+
+        x = self._conv(x, 64, istraining)
+        x = self._conv(x, 64, istraining)
+
+        x = tf.image.resize_images(x, (shape4[1], shape4[2]), method = tf.image.ResizeMethod.BILINEAR)
+        x = tf.keras.layers.Concatenate()([x, conv4])
+        x = self._conv(x, 64, istraining)
+        x = self._conv(x, 64, istraining)
+
+        x = tf.image.resize_images(x, (shape3[1], shape3[2]), method = tf.image.ResizeMethod.BILINEAR)
+        x = tf.keras.layers.Concatenate()([x, conv3])
+        x = self._conv(x, 64, istraining)
+        x = self._conv(x, 64, istraining)        
+
+        x = tf.image.resize_images(x, (shape2[1], shape2[2]), method = tf.image.ResizeMethod.BILINEAR)
+        x = tf.keras.layers.Concatenate()([x, conv2])
+        x = self._conv(x, 64, istraining)
+        x = self._conv(x, 64, istraining)
+
+        x = tf.image.resize_images(x, (shape1[1], shape1[2]), method = tf.image.ResizeMethod.BILINEAR)
+        x = tf.keras.layers.Concatenate()([x, conv1])
+        x = self._conv(x, 64, istraining)
+        x = self._conv(x, 64, istraining)
+
+        x = tf.keras.layers.Conv2D(1, (3, 3), padding = "same")(x)
+
+        return x
+
+    def compile(self):
+        input_tensor = tf.placeholder(tf.float32, [None, None, None, 3])
+        label_tensor = tf.placeholder(tf.float32, [None, None, None, 1])
+
+        pred = self.build(input_tensor, True)
+        output = tf.nn.sigmoid(pred)
+        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=pred, labels=label_tensor))
+
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            optim = tf.train.AdamOptimizer().minimize(loss)
+        return input_tensor, label_tensor, output, loss, optim
+
+    def worker(self, info):
+        if len(info.strip().split("."))!=2:
+            return None
+        fname, suffix = info.strip().split(".")
+        img_path = self.data_path+"Images/"+fname+".jpg"
+        label_path = self.data_path+"train_gt_t13/"+fname+".txt"
+        img, label_img = util.load_data(img_path, label_path)
+        if img is None:
+            return None
+        # img, label_img = util.random_crop_resize(img, label_img, [256, 256])
+        return (img, label_img, fname)
+
+    def train(self):
+        input_tensor, label_tensor, output, loss, optim = self.compile()
+
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep = 1)
+        restore_path = "checkpoint/"
+
+        self.data_path = "../data/icdar2019/"
+
+        data_list = os.listdir(self.data_path+"train_gt_t13/")
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            checkpoint = tf.train.latest_checkpoint(restore_path)
+
+            if checkpoint:
+                print("restore from: " + checkpoint)
+                saver.restore(sess, checkpoint)
+
+            for epoch in range(100):
+                pool = multiprocessing.Pool(1)
+                batch_size = 8
+                batch_data = []
+                batch_label = []
+                cnt=0
+                for r in pool.imap_unordered(self.worker, data_list):
+                    if r is None:
+                        continue
+                    img, label_img, fname = r
+                    print(fname)
+                    batch_data.append(img)
+                    batch_label.append(label_img)
+                    if len(batch_data)>=batch_size:
+                        cnt+=1
+                        batch_data = np.array(batch_data)
+                        batch_label = np.array(batch_label).reshape([8, 256, 256, 1])
+                        _, l, o = sess.run([optim, loss, output], feed_dict={input_tensor:batch_data, label_tensor:batch_label})
+                        print(l)
+                        if cnt%3==0:
+                            out_img = o[-1, :, :, :]
+                            cv2.imwrite("output.jpg", out_img.astype(np.uint8)*255)
+                            saver.save(sess, os.path.join(restore_path, 'model'))
+                            cv2.imwrite("label.jpg", batch_label[-1,:,:,:].astype(np.uint8)*255)
+                            cv2.imwrite("input.jpg", batch_data[-1,:,:,:].astype(np.uint8))
+                        batch_data = []
+                        batch_label = []
+
+
+if __name__=="__main__":
+    unet = Unet()
+    unet.train()
---
+++ util.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+import os
+import random
+
+def point_in_box(x, y, box):
+    y1, x1, y2, x2, y3, x3, y4, x4 = box
+    a = (x2-x1)*(y-y1)-(y2-y1)*(x-x1)
+    b = (x3-x2)*(y-y2)-(y3-y2)*(x-x2)
+    c = (x4-x3)*(y-y3)-(y4-y3)*(x-x3)
+    d = (x1-x4)*(y-y4)-(y1-y4)*(x-x4)
+
+    return (a>=0 and b>=0 and c>=0 and d>=0) or (a<=0 and b<=0 and c<=0 and d<=0)
+
+def load_data(img_path, label_path):
+    img = cv2.imread(img_path)
+    if img is None:
+        return None, None
+    min_l = float(min(img.shape[0], img.shape[1]))
+    rate= 256.0/min_l
+    img = cv2.resize(img, (0,0), fx = rate, fy = rate, interpolation=cv2.INTER_LINEAR)
+    offsetx = random.randint(0, img.shape[0]-256)
+    offsety = random.randint(0, img.shape[1]-256)
+    img = img[offsetx:offsetx+256, offsety:offsety+256,:]
+
+    boxes = []
+    with open(label_path, "r", encoding="utf-8") as f:
+        for line in f:
+            segs = line.strip().split(",")
+            box = list(map(lambda x:float(x)*rate, segs[:8]))
+            for i in range(len(box)):
+                if i&1==0:
+                    box[i]-=offsety
+                else:
+                    box[i]-=offsetx
+            boxes.append(box)
+
+    label_img = np.zeros([img.shape[0],img.shape[1],1])
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            for box in boxes:
+                if point_in_box(x, y, box):
+                    label_img[x, y, 0]=1
+                    break
+    return img, label_img
+
+# def random_crop_resize(img, label_img, shape=[256, 256]):
+#     l = min(img.shape[0], img.shape[1])
+#     offsetx = random.randint(0, img.shape[0]-l)
+#     offsety = random.randint(0, img.shape[1]-l)
+
+#     img, label_img = img[offsetx:offsetx+l, offsety:offsety+l,:], label_img[offsetx:offsetx+l, offsety:offsety+l,:]
+
+#     img = cv2.resize(img, (shape[1], shape[0]), interpolation=cv2.INTER_LINEAR)
+#     label_img = cv2.resize(label_img, (shape[1], shape[0]), interpolation=cv2.INTER_NEAREST)
+#     return img, label_img
